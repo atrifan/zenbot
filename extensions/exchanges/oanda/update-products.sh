3cd5be27680ba3ce18970f5507ad2d25575ebf5f
trifan <trifan@adobe.com>
2021-03-25 20:19:43 +0200

    small update

--- extensions/exchanges/oanda/update-products.sh
+++ extensions/exchanges/oanda/update-products.sh
@@ -5,20 +5,16 @@ new OandaApi().fetchMarkets().then((markets) => {
     var products = []
 
     var products = markets.map((market) => {
-        console.log(market);
         return {
-            name: market.name,
-            type: market.type,
-            displayName: market.displayName,
-            pipLocation: market.pipLocation,
-            displayPrecision: market.displayPrecision,
-            tradeUnitsPrecision: market.tradeUnitsPrecision,
-            minimumTradeSize: market.minimumTradeSize,
-            maximumTrailingStopDistance: market.maximumTrailingStopDistance,
-            minimumTrailingStopDistance: market.minimumTrailingStopDistance,
-            maximumPositionSize: market.maximumPositionSize,
-            maximumOrderUnits: market.maximumOrderUnits,
-            marginRate: market.marginRate
+            id: market.name,
+            asset: market.name.split("_")[0],
+            currency: market.name.split("_")[1],
+            min_size: market.minimumTradeSize,
+            max_size: market.maximumOrderUnits,
+            min_total: 0,
+            increment: Number((10**market.pipLocation).toFixed(9)),
+            asset_increment: Number((10**(-market.displayPrecision)).toFixed(9)),
+            label: market.displayName
         };
     });
 
